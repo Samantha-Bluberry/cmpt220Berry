8666b0b4355ded8031d7bce54fdf1ed172196155
Samantha Berry <Samantha.Berry1@marist.edu>
2017-01-18 21:22:22 -0500

    added files for project 0

---
+++ projects/prj0/Driver_prj0.java
@@ -0,0 +1,15 @@
+/**
+ * file: Driver_prj0.java
+ * author: Samantha Berry
+ * course: CMPT 220
+ * assignment: project 0
+ * due date: January 24, 2017
+ * version 1.0
+ * 
+ * This file prints out the words "Hello World!"
+ */
+public class Driver_prj0 {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  } //prints out "Hello World!"
+}
